2b9c563a0ae4fb3aed8e3829d54cf6b62bb36395
programmerMrleader <wayyean98@gmail.com>
2026-08-20 17:51:57 +0630

    Remove Some Credentials

---
+++ README.md
@@ -0,0 +1,2 @@
+This is the telegram bot that I develop with esp32 with wifi and LCD :D
+Hope you enjoy this :D
--- include/settings.h
+++ include/settings.h
@@ -4,11 +4,11 @@
 
 #include <LiquidCrystal.h>
 #include <string>// define the wifi
-#define WIFI_SSID "Kaung Myat Thu"//"Chessy Guy"
-#define WIFI_PASSWORD "441796487"//"waiyeanhein0098765@#!$"
+#define WIFI_SSID "WIFI_SSID"//""
+#define WIFI_PASSWORD "WIFI_PASSWORD"//""
 // Telegram BOT Token (Get from Botfather)
-#define BOT_TOKEN "8890339447:AAGxJhTSK7QdhyPGpT-ehnpjZazrKFSBruc"
-#define UNI_SPLASH_TOKEN "RpHOfOma9J0uXIi0v2zDq1kYRFkdfbXVKGxf68ymyeQ"
+#define BOT_TOKEN "YOUR_BOT_TOKEN"
+#define UNI_SPLASH_TOKEN "YOUR_UNI_SPLASH_TOKEN"
 
 // declearing constant
 inline const unsigned long BOT_MTBS = 1000; // mean time between scan messages
